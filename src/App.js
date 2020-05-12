8e24f437c42d049fc463beb00cb09631a8b3415c
Seanny Phoenix <seannyphoenix@gmail.com>
2020-05-12 13:17:18 -0700

    Initial React App Setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './scss/custom.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <h1>Seanny Drakon Phoenix</h1>
+        </div>
+        <div className="card-body">
+          <p>Making sure it works</p>
+        </div>
+        <div className="card-footer">
+          <small>Bye. ~Seanny Drakon Phoenix</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
